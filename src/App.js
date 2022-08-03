f71ff1a10a43b69d2d3abf7795bf36f7a32ecdc5
prospero <prospero1135@gmail.com>
2022-08-04 02:27:53 +0300

    dagre layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import ForceTree from "./components/ForceTree/ForceTree";
-import Generations from "./components/Generations/Generations";
 import Tree from "./components/Tree/Tree";
 
 function App() {
   return (
     <div className="App">
-      <Generations />
-      {/* <Tree /> */}
-      <ForceTree />
+      <Tree />
     </div>
   );
 }
--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -1,12 +1,54 @@
+import React from "react";
 import useTree from "./useTree";
 import "./tree.css";
 
 const Tree = () => {
-  const { svgRef, canvaSize } = useTree();
+  const { nodeDataList, graphDimensions, nodeDimensions, linkList, stepLine } =
+    useTree();
 
   return (
     <div className="tree">
-      <svg ref={svgRef} width={canvaSize.width} height={canvaSize.height} />
+      <svg width={graphDimensions.width} height={graphDimensions.height}>
+        {/* links group */}
+        <g>
+          {linkList.map((link, idx) => {
+            return (
+              <path
+                key={idx}
+                d={stepLine(link)}
+                fill="transparent"
+                stroke="green"
+              ></path>
+            );
+          })}
+        </g>
+        {/* nodes group */}
+        <g>
+          {nodeDataList.map((node, idx) => {
+            return (
+              <g key={idx}>
+                <rect
+                  height={nodeDimensions.height}
+                  width={nodeDimensions.width}
+                  rx={nodeDimensions.radius}
+                  ry={nodeDimensions.radius}
+                  x={node.x - nodeDimensions.width / 2}
+                  y={node.y - nodeDimensions.height / 2}
+                  className="tree__node"
+                ></rect>
+                <text
+                  x={node.x}
+                  y={node.y}
+                  dominantBaseline="middle"
+                  textAnchor="middle"
+                >
+                  {node.label}
+                </text>
+              </g>
+            );
+          })}
+        </g>
+      </svg>
     </div>
   );
 };
